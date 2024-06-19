81f5423898f500315bffc7b3a3858f1544b57724
jaewoo9797 <9712jw@gmail.com>
2024-06-19 10:39:42 +0900

    db connection test

--- src/test/java/com/kosa/realestate/RealEstateSaleHistoryApplicationTests.java
+++ src/test/java/com/kosa/realestate/RealEstateSaleHistoryApplicationTests.java
@@ -1,13 +1,26 @@
 package com.kosa.realestate;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class RealEstateSaleHistoryApplicationTests {
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  public void testConnection() {
+    assertThat(jdbcTemplate).isNotNull();
+  }
+
+  @Test
+  void contextLoads() {
+    String sql = "SELECT 1 FROM DUAL";
+    Integer result = jdbcTemplate.queryForObject(sql, Integer.class);
+    assertThat(result).isEqualTo(1);
+  }
 
 }
