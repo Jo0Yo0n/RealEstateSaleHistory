42b21127674a182554e8e7c963e8034a1a8a5c46
kangansoo <ansoo971@gmail.com>
2024-06-21 18:05:00 +0900

    Feat:디테일 페이지 매매기록 리스트 출력

--- src/test/java/com/kosa/realestate/RealEstateDetailTest.java
+++ src/test/java/com/kosa/realestate/RealEstateDetailTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.kosa.realestate.realestates.model.RealEstateSale;
 import com.kosa.realestate.realestates.model.RealEstateWithSale;
 import com.kosa.realestate.realestates.service.IRealEstateDetailService;
 
@@ -16,5 +17,11 @@ class RealEstateDetailTest {
   void detailTest() {
     List<RealEstateWithSale> realEstateDetail = realEstateDetailService.getRealEstateDetail(7723);
     System.out.println(realEstateDetail);
+  }
+  
+  @Test
+  void detailPriceTest() {
+    List<RealEstateSale> realEstatePirce = realEstateDetailService.getRealEstatePrice(3313);
+    System.out.println(realEstatePirce);
   }
 }
