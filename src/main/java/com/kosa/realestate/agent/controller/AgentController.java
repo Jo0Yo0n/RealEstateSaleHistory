8facc71ae6e775b56d7a7feaeace607ac0526b58
kangansoo <ansoo971@gmail.com>
2024-06-27 16:56:09 +0900

    Design:중개사 마이페이지, 매물 등록 페이지 틀 완성

--- src/main/java/com/kosa/realestate/agent/controller/AgentController.java
+++ src/main/java/com/kosa/realestate/agent/controller/AgentController.java
@@ -1,10 +1,17 @@
 package com.kosa.realestate.agent.controller;
 
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.kosa.realestate.admindivision.model.dto.CityDTO;
+import com.kosa.realestate.admindivision.model.dto.DistrictDTO;
+import com.kosa.realestate.admindivision.model.dto.NeighborhoodDTO;
+import com.kosa.realestate.admindivision.service.IAdminDivisionService;
 import com.kosa.realestate.agent.model.AgentCreateFormDTO;
 import com.kosa.realestate.agent.service.IAgentService;
 import jakarta.validation.Valid;
@@ -15,25 +22,51 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/agent")
 public class AgentController {
   private final IAgentService agentService;
-  
+  private final IAdminDivisionService admindivisionService;
+
+  @GetMapping("/agentpage")
+  public String agentPage(AgentCreateFormDTO form) {
+    return "agent-page-tab";
+  }
+
   @GetMapping("/createform")
   public String createForm(AgentCreateFormDTO form) {
-    return "agent_create_form";
+    return "create-sales-form";
   }
-  
+
   @PostMapping("/createform")
   public String createForm(@Valid AgentCreateFormDTO form, BindingResult bindingResult) {
     if (bindingResult.hasErrors()) {
-        return "signup_form";
+      return "create-sales-form";
     }
-    
+
     Integer realEstateId = agentService.getRealEstateId(form.getAddressStreet());
-    
+
     if (realEstateId == null) {
       agentService.insertRealEstate(form); // 도로명 주소로 매물이 없으면 삽입
-      realEstateId = agentService.getRealEstateId(form.getAddressStreet()); // 삽입 후 realEstateId 다시 가져오기
+      realEstateId = agentService.getRealEstateId(form.getAddressStreet()); // 삽입 후 realEstateId 다시
+                                                                            // 가져오기
     }
-    
-    return "signup_form";
+
+    return "create-sales-form";
+  }
+
+  // 시 리스트를 제공하는 엔드포인트
+  @GetMapping("/cities")
+  public ResponseEntity<List<CityDTO>> cityList() {
+    return ResponseEntity.ok(admindivisionService.findCityList());
+  }
+
+  // 구 리스트를 제공하는 엔드포인트
+  @GetMapping("/cities/districts")
+  public ResponseEntity<List<DistrictDTO>> districtList(@RequestParam("cityId") Long cityId) {
+    return ResponseEntity.ok(admindivisionService.findDistrictList(cityId));
+  }
+
+  // 동 리스트를 제공하는 엔드포인트
+  @GetMapping("/districts/neighborhoods")
+  public ResponseEntity<List<NeighborhoodDTO>> neighborhoodList(
+      @RequestParam("districtId") Long districtId) {
+    return ResponseEntity.ok(admindivisionService.findNeighborhoodList(districtId));
   }
 };
