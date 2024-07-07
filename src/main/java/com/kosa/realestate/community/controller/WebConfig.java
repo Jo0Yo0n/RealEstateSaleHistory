776e371fc49b380dcf7076e3cc743911503c33df
KangHeew <144186976+KangHeew@users.noreply.github.com>
2024-07-08 02:46:09 +0900

    주석

--- src/main/java/com/kosa/realestate/community/controller/WebConfig.java
+++ src/main/java/com/kosa/realestate/community/controller/WebConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * WebConfig 클래스
+ *
+ * @author 강희원
+ */
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
