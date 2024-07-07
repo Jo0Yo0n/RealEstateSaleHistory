29b45239c1f4d25778e9259fe3883bb336076c09
KangHeew <144186976+KangHeew@users.noreply.github.com>
2024-07-08 02:47:58 +0900

    주석 PostInfoDTO.java

--- src/main/java/com/kosa/realestate/community/dto/PostInfoDTO.java
+++ src/main/java/com/kosa/realestate/community/dto/PostInfoDTO.java
@@ -3,6 +3,12 @@ package com.kosa.realestate.community.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * PostInfoDTO 클래스
+ * 
+ * @author 강희원
+ */
+
 @Getter
 @Setter
 public class PostInfoDTO {
